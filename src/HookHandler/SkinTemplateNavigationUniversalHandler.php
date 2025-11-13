5ca391e7eba090b690697ad9a8673a867d65a065
Dillon Hardy <dhardy@wikimedia.org>
2025-11-13 12:58:40 -0500

    Implement final design of Dashboard menu link
    - Change the link icon to `viewCompact` using resource module
    - Assign a named key to the `user-menu` link
    - Remove redundant `BeforePageDisplayHandler` hook
    - Add `personal-dashboard-link-title` to i18n messages
    
    Bug: T405745
    Change-Id: I1595a4f6f359002bc7d483fee8ffe1b738f0904f

--- src/HookHandler/SkinTemplateNavigationUniversalHandler.php
+++ src/HookHandler/SkinTemplateNavigationUniversalHandler.php
@@ -1,15 +1,11 @@
 <?php
-// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
 
 namespace MediaWiki\Extension\PersonalDashboard\HookHandler;
 
 use MediaWiki\Hook\SkinTemplateNavigation__UniversalHook;
 use MediaWiki\SpecialPage\SpecialPageFactory;
 
-class SkinTemplateNavigationUniversalHandler implements
-	SkinTemplateNavigation__UniversalHook
-{
-
+class SkinTemplateNavigationUniversalHandler implements SkinTemplateNavigation__UniversalHook {
 	private SpecialPageFactory $specialPageFactory;
 
 	public function __construct( SpecialPageFactory $specialPageFactory ) {
@@ -19,12 +15,29 @@ class SkinTemplateNavigationUniversalHandler implements
 	/**
 	 * @inheritDoc
 	 */
-	public function onSkinTemplateNavigation__Universal( $skinTemplate, &$links ): void {
-		$specialTitle = $this->specialPageFactory->getTitleForAlias( 'PersonalDashboard' );
-		$links['user-menu'][] = [
-			'text' => $specialTitle->getText(),
-			'href' => $specialTitle->getFullURL(),
-			'icon' => 'newspaper',
+	public function onSkinTemplateNavigation__Universal( $sktemplate, &$links ): void {
+		if ( !$sktemplate->getUser()->isNamed() ) {
+			return;
+		}
+
+		$menu = &$links['user-menu'];
+		$link = [
+			'text' => $sktemplate->msg( 'personal-dashboard-link-title' ),
+			'href' => $this->specialPageFactory->getTitleForAlias( 'PersonalDashboard' )->getFullURL(),
+			'icon' => 'viewCompact'
 		];
+
+		// Insert before the "Preferences" link, or fallback to end if it doesn't exist
+		$offset = array_search( 'preferences', array_keys( $menu ) );
+
+		if ( $offset === false ) {
+			$menu['personaldashboard'] = $link;
+		} else {
+			$menu = array_slice( $menu, 0, $offset, true ) +
+				[ 'personaldashboard' => $link ] +
+				array_slice( $menu, $offset, null, true );
+		}
+
+		$sktemplate->getOutput()->addModuleStyles( 'ext.personalDashboard.menuIcon' );
 	}
 }
--- tests/phpunit/unit/HookHandler/SkinTemplateNavigationUniversalHandlerTest.php
+++ tests/phpunit/unit/HookHandler/SkinTemplateNavigationUniversalHandlerTest.php
@@ -3,39 +3,67 @@
 namespace MediaWiki\Extension\PersonalDashboard\Tests\Unit\HookHandler;
 
 use MediaWiki\Extension\PersonalDashboard\HookHandler\SkinTemplateNavigationUniversalHandler;
+use MediaWiki\Output\OutputPage;
 use MediaWiki\Skin\SkinTemplate;
 use MediaWiki\SpecialPage\SpecialPageFactory;
 use MediaWiki\Title\Title;
+use MediaWiki\User\User;
 use MediaWikiUnitTestCase;
 
 /**
  * @coversDefaultClass \MediaWiki\Extension\PersonalDashboard\HookHandler\SkinTemplateNavigationUniversalHandler
  */
 class SkinTemplateNavigationUniversalHandlerTest extends MediaWikiUnitTestCase {
+	private User $userMock;
+	private SkinTemplate $skinTemplateMock;
+	private SkinTemplateNavigationUniversalHandler $handler;
+
+	private string $text = 'personal-dashboard-link-title';
+	private string $href = 'https://example.com/test';
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$outputPageMock = $this->createMock( OutputPage::class );
+		$this->userMock = $this->createMock( User::class );
+
+		$this->skinTemplateMock = $this->createMock( SkinTemplate::class );
+		$this->skinTemplateMock->method( 'getOutput' )->willReturn( $outputPageMock );
+		$this->skinTemplateMock->method( 'getUser' )->willReturn( $this->userMock );
+		$this->skinTemplateMock->method( 'msg' )->willReturnArgument( 0 );
+
+		$titleMock = $this->createMock( Title::class );
+		$titleMock->method( 'getFullURL' )->willReturn( $this->href );
+
+		$specialPageFactoryMock = $this->createMock( SpecialPageFactory::class );
+		$specialPageFactoryMock->method( 'getTitleForAlias' )->willReturn( $titleMock );
+
+		$this->handler = new SkinTemplateNavigationUniversalHandler( $specialPageFactoryMock );
+	}
 
 	/**
 	 * @covers ::onSkinTemplateNavigation__Universal
 	 */
-	public function testOnSkinTemplateNavigation__Universal() {
-		$skinTemplateMock = $this->createNoOpMock( SkinTemplate::class );
-		$links = [];
+	public function testLinkExistsIfNamedUser() {
+		$links = [ 'user-menu' => [] ];
 
-		$text = 'TestText';
-		$href = 'http://example.com/test';
-		$titleMock = $this->createMock( Title::class );
-		$titleMock->method( 'getText' )->willReturn( $text );
-		$titleMock->method( 'getFullURL' )->willReturn( $href );
-		$specialPageFactoryMock = $this->createMock( SpecialPageFactory::class );
-		$specialPageFactoryMock->expects( $this->once() )->method( 'getTitleForAlias' )->willReturn( $titleMock );
-		$handler = new SkinTemplateNavigationUniversalHandler( $specialPageFactoryMock );
-		$handler->onSkinTemplateNavigation__Universal( $skinTemplateMock, $links );
+		$this->userMock->method( 'isNamed' )->willReturn( true );
+		$this->handler->onSkinTemplateNavigation__Universal( $this->skinTemplateMock, $links );
 
-		$this->assertArrayHasKey( 'user-menu', $links );
-		$testNavLink = $links['user-menu'][0];
-		$this->assertArrayHasKey( 'text', $testNavLink );
-		$this->assertArrayHasKey( 'href', $testNavLink );
-		$this->assertArrayHasKey( 'icon', $testNavLink );
-		$this->assertEquals( $text, $testNavLink['text'] );
-		$this->assertEquals( $href, $testNavLink['href'] );
+		$link = $links['user-menu']['personaldashboard'];
+		$this->assertEquals( $this->text, $link['text'] );
+		$this->assertEquals( $this->href, $link['href'] );
+	}
+
+	/**
+	 * @covers ::onSkinTemplateNavigation__Universal
+	 */
+	public function testLinkNotExistsIfNotNamedUser() {
+		$links = [ 'user-menu' => [] ];
+
+		$this->userMock->method( 'isNamed' )->willReturn( false );
+		$this->handler->onSkinTemplateNavigation__Universal( $this->skinTemplateMock, $links );
+
+		$this->assertArrayNotHasKey( 'personaldashboard', $links['user-menu'] );
 	}
 }
